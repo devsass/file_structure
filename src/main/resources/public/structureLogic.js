b1f7a8b6b42d7fb7e902d20f9ee4e75da4a92c3e
devsass <a.sassaerialvideo@gmail.com>
2016-04-10 16:31:21 -0700

    i commited

--- src/main/resources/public/structureLogic.js
+++ src/main/resources/public/structureLogic.js
@@ -12,17 +12,22 @@ function createFolder (divID) {
     var buttonName = 'button' + bCount;
     bCount += 1;
     var div = createDiv(buttonName + "div");
-    document.body.appendChild(div);
-    var img = createImg(buttonName + "img", "14", "20", "index.jpg");
-    document.body.appendChild(img);
+    var filesDiv = createDiv(buttonName + "div" + "files");
+    var img = createImg(buttonName + "img", "14", "20", "index.png");
     var name = document.createTextNode(buttonName);
     var button = document.createElement("BUTTON");
+    var dropB = createDropButton(buttonName + "drop", filesDiv.getAttribute("id"));
+    var addB = createAddButton(buttonName + "add", filesDiv.getAttribute("id"));
     button.appendChild(name);
     document.body.appendChild(button);
     button.setAttribute("id", buttonName);
     button.setAttribute("class", "fileStyle");
     button.addEventListener("dblclick", openFile);
-    div.appendChild(img); div.appendChild(button);
+    div.appendChild(dropB);
+    div.appendChild(img);
+    div.appendChild(button);
+    div.appendChild(addB);
+    div.appendChild(filesDiv);
     document.getElementById(divID).appendChild(div);
 }
 
@@ -30,8 +35,7 @@ function createFile (divID) {
     var buttonName = 'button' + bCount;
     bCount += 1;
     var div = createDiv(buttonName + "div");
-    document.body.appendChild(div);
-    //var img = createImg(buttonName + "img", "14", "20", "index.jpg");
+    //var img = createImg(buttonName + "img", "14", "20", "index.png");
     //document.body.appendChild(img);
     var name = document.createTextNode(buttonName);
     var button = document.createElement("BUTTON");
@@ -45,12 +49,33 @@ function createFile (divID) {
     document.getElementById(divID).appendChild(div);
 }
 
+function createDropButton(id, divId) {
+    var dropB = document.createElement("BUTTON");
+    dropB.innerHTML = ">";
+    dropB.setAttribute("id", id);
+    dropB.setAttribute("onclick", "toggleDisplay('"+ divId + "','" + id + "');");
+    dropB.setAttribute("class", "dropdown");
+    document.body.appendChild(dropB);
+    return dropB;
+}
+
+function createAddButton(id, param) {
+    var addB = document.createElement("BUTTON");
+    addB.innerHTML = (createImg(id+"img", 10, 10, "add_file.png")).outerHTML;
+    addB.setAttribute("id", id);
+    addB.setAttribute("class", "addButton")
+    addB.setAttribute("onclick", "createFile('" + param + "')");
+    document.body.appendChild(addB);
+    return addB;
+}
+
 function createImg (id, hi, wi, imgfile) {
     var img = document.createElement("img");
     img.setAttribute("id", id);
     img.setAttribute("height", hi);
     img.setAttribute("width", wi);
     img.setAttribute("src", imgfile);
+    document.body.appendChild(img);
     return img;
 }
 
@@ -58,6 +83,7 @@ function createDiv(id) {
     var div = document.createElement("div");
     div.setAttribute("id", id);
     div.setAttribute("class", "folder");
+    document.body.appendChild(div);
     return div;
 }
 
