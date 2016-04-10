c5abb4306c3d341480716cc6c822bd8aa9e94655
devsass <a.sassaerialvideo@gmail.com>
2016-04-10 14:01:16 -0700

    i commited

--- src/main/resources/public/structureLogic.js
+++ src/main/resources/public/structureLogic.js
@@ -8,18 +8,58 @@ var newfile = document.getElementById('newfile');
 var bCount = 1;
 
 
-function createButton (divID) {
+function createFolder (divID) {
     var buttonName = 'button' + bCount;
     bCount += 1;
+    var div = createDiv(buttonName + "div");
+    document.body.appendChild(div);
+    var img = createImg(buttonName + "img", "14", "20", "index.jpg");
+    document.body.appendChild(img);
     var name = document.createTextNode(buttonName);
     var button = document.createElement("BUTTON");
     button.appendChild(name);
     document.body.appendChild(button);
     button.setAttribute("id", buttonName);
+    button.setAttribute("class", "fileStyle");
     button.addEventListener("dblclick", openFile);
-    document.getElementById(divID).appendChild(button);
+    div.appendChild(img); div.appendChild(button);
+    document.getElementById(divID).appendChild(div);
 }
 
+function createFile (divID) {
+    var buttonName = 'button' + bCount;
+    bCount += 1;
+    var div = createDiv(buttonName + "div");
+    document.body.appendChild(div);
+    //var img = createImg(buttonName + "img", "14", "20", "index.jpg");
+    //document.body.appendChild(img);
+    var name = document.createTextNode(buttonName);
+    var button = document.createElement("BUTTON");
+    button.appendChild(name);
+    document.body.appendChild(button);
+    button.setAttribute("id", buttonName);
+    button.setAttribute("class", "fileStyle");
+    button.addEventListener("dblclick", openFile);
+    //div.appendChild(img);
+    div.appendChild(button);
+    document.getElementById(divID).appendChild(div);
+}
+
+function createImg (id, hi, wi, imgfile) {
+    var img = document.createElement("img");
+    img.setAttribute("id", id);
+    img.setAttribute("height", hi);
+    img.setAttribute("width", wi);
+    img.setAttribute("src", imgfile);
+    return img;
+}
+
+function createDiv(id) {
+    var div = document.createElement("div");
+    div.setAttribute("id", id);
+    div.setAttribute("class", "folder");
+    return div;
+}
 
 function rightClick(){
 
